2cb66a2601c4d827fcf1c5dce42dee5548ef47c6
sayan801 <sayan801@gmail.com>
2025-08-22 16:30:59 +0530

    first commit

---
+++ src/main/java/com/technicise/fhirfacade/FhirfacadeApplication.java
@@ -0,0 +1,13 @@
+package com.technicise.fhirfacade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhirfacadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhirfacadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technicise/fhirfacade/FhirfacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technicise.fhirfacade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhirfacadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
