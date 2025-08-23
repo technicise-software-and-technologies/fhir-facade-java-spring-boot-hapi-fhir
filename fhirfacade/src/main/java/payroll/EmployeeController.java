26617c5c3e0f0f331a558b9e5c122c7d75c91fda
atanu-technicise <atanupandit.technicise@gmail.com>
2025-08-23 16:43:16 +0530

    postman and post route updated

--- fhirfacade/src/main/java/payroll/EmployeeController.java
+++ fhirfacade/src/main/java/payroll/EmployeeController.java
@@ -23,86 +23,83 @@ import ca.uhn.fhir.rest.client.api.IGenericClient;
 
 // tag::hateoas-imports[]
 // end::hateoas-imports[]
-
 @RestController
 class EmployeeController {
 
-	private final EmployeeRepository repository;
+    private final EmployeeRepository repository;
 
-	EmployeeController(EmployeeRepository repository) {
-		this.repository = repository;
-	}
+    EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
+    }
 
-	// Aggregate root
+    String baseURL = "http://localhost:8080/fhir";
 
-	// tag::get-aggregate-root[]
-	@GetMapping("/employees")
-	CollectionModel<EntityModel<Employee>> all() {
+    FhirContext ctx = FhirContext.forR4();
 
-		List<EntityModel<Employee>> employees = repository.findAll().stream()
-				.map(employee -> EntityModel.of(employee,
-						linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
-						linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
-				.collect(Collectors.toList());
+    IGenericClient client = ctx.newRestfulGenericClient(baseURL);
 
-		return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
-	}
-	// end::get-aggregate-root[]
+    // Aggregate root
+    // tag::get-aggregate-root[]
+    @GetMapping("/employees")
+    CollectionModel<EntityModel<Employee>> all() {
 
-	@PostMapping("/employees")
-	Employee newEmployee(@RequestBody Employee newEmployee) {
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
 
-		String baseURL = "http://localhost:8080/fhir";
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+    }
+    // end::get-aggregate-root[]
 
-		FhirContext ctx = FhirContext.forR4();
+    @PostMapping("/employees")
+    Employee newEmployee(@RequestBody Employee newEmployee) {
 
-		IGenericClient client = ctx.newRestfulGenericClient(baseURL);
+        Patient patient = new Patient();
 
-		Patient patient = new Patient();
-
-		String[] nameParts = newEmployee.getName().split(" ");
-
-		patient.addName().addGiven(nameParts[0]).setFamily(nameParts[1]);
-		patient.setActive(true);
+        String[] nameParts = newEmployee.getName().split(" ");
+        
+        patient.addName().addGiven(nameParts[0]).setFamily(nameParts[1]);
+        patient.setActive(true);
 		patient.setGender(AdministrativeGender.UNKNOWN);
-		// patient.addTelecom().setSystem(ContactPointSystem.PHONE).setValue("1234567890");
+        // patient.addTelecom().setSystem(ContactPointSystem.PHONE).setValue("1234567890");
 
-		MethodOutcome outcome = client.create().resource(patient).prettyPrint().encodedJson().execute();
-		
+        MethodOutcome outcome = client.create().resource(patient).prettyPrint().encodedJson().execute();
+    	
 		return repository.save(newEmployee);
-	}
-
-	// Single item
-
-	// tag::get-single-item[]
+    }
+    // Single item
+    
+    // tag::get-single-item[]
 	@GetMapping("/employees/{id}")
-	EntityModel<Employee> one(@PathVariable Long id) {
-
+        ntityModel<Employee> one(@PathVariable Long id) {
+                
 		Employee employee = repository.findById(id) //
-				.orElseThrow(() -> new EmployeeNotFoundException(id));
-
-		return EntityModel.of(employee, //
-				linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
-				linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+        		.orElseThrow(() -> new EmployeeNotFoundException(id));
+                
+                turn EntityModel.of(employee, //
+    			linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+    			linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
 	}
-	// end::get-single-item[]
-
+    // end::get-single-item[]
+    
 	@PutMapping("/employees/{id}")
-	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-
-		return repository.findById(id) //
-				.map(employee -> {
-					employee.setName(newEmployee.getName());
-					employee.setRole(newEmployee.getRole());
-					return repository.save(employee);
-				}) //
-				.orElseGet(() -> {
-					return repository.save(newEmployee);
+        mployee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+                
+                    urn repository.findById(id) //
+                    map(employee -> {
+                    employee.setName(newEmployee.getName());
+                	employee.setRole(newEmployee.getRole());
+                	return repository.save(employee);
+                    ) //
+                .orElseGet(() -> {
+    				return repository.save(newEmployee);
 				});
-	}
-
-	@DeleteMapping("/employees/{id}")
-	void deleteEmployee(@PathVariable Long id) {
+    }
+    
+        DeleteMapping("/employees/{id}")
+    void deleteEmployee(@PathVariable Long id) {
 		repository.deleteById(id);
 	}
 }
