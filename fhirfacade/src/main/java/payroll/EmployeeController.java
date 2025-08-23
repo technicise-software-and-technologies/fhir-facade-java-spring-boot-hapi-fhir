c267d36358633d23158b7c0b898bae4e845360b3
sayan801 <sayan801@gmail.com>
2025-08-23 15:16:29 +0530

    creating employee in fhir

--- fhirfacade/src/main/java/payroll/EmployeeController.java
+++ fhirfacade/src/main/java/payroll/EmployeeController.java
@@ -1,12 +1,14 @@
 package payroll;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.hl7.fhir.r4.model.Enumerations.AdministrativeGender;
+import org.hl7.fhir.r4.model.Patient;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.hateoas.EntityModel;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.MethodOutcome;
+import ca.uhn.fhir.rest.client.api.IGenericClient;
 
 // tag::hateoas-imports[]
 // end::hateoas-imports[]
@@ -45,6 +51,24 @@ class EmployeeController {
 
 	@PostMapping("/employees")
 	Employee newEmployee(@RequestBody Employee newEmployee) {
+
+		String baseURL = "https://localhost:5001/fhir";
+
+		FhirContext ctx = FhirContext.forR4();
+
+		IGenericClient client = ctx.newRestfulGenericClient(baseURL);
+
+		Patient patient = new Patient();
+
+		String[] nameParts = newEmployee.getName().split(" ");
+
+		patient.addName().addGiven(nameParts[0]).setFamily(nameParts[1]);
+		patient.setActive(true);
+		patient.setGender(AdministrativeGender.UNKNOWN);
+		// patient.addTelecom().setSystem(ContactPointSystem.PHONE).setValue("1234567890");
+
+		MethodOutcome outcome = client.create().resource(patient).prettyPrint().encodedJson().execute();
+		
 		return repository.save(newEmployee);
 	}
 
--- fhirfacade/src/main/java/payroll/LoadDatabase.java
+++ fhirfacade/src/main/java/payroll/LoadDatabase.java
@@ -15,8 +15,8 @@ class LoadDatabase {
 	CommandLineRunner initDatabase(EmployeeRepository repository) {
 
 		return args -> {
-			log.info("Preloading " + repository.save(new Employee("Bilbo Baggins", "burglar")));
-			log.info("Preloading " + repository.save(new Employee("Frodo Baggins", "thief")));
+			log.info(" Not loading 1 "); //+ repository.save(new Employee("Bilbo Baggins", "burglar")));
+			log.info(" Not loading 2 "); //+ repository.save(new Employee("Frodo Baggins", "thief")));
 		};
 	}
 }
